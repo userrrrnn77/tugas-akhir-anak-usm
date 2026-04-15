8c0c84d0d484c873d915d4f6770ef0fae2c0c412
Renss <rendyharvest@gmail.com>
2026-04-15 23:10:35 +0700

    api

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,8 +1,10 @@
 // src/api/axios.ts
 import axios from "axios";
 
+const baseURl = import.meta.env.VITE_CORE_URL
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_CORE_URL,
+  baseURL: baseURl,
   headers: {
     "Content-Type": "application/json",
   },
